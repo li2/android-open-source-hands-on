04b295a32f22fbfd519983048e87231af77c38fa
weiyi.li <weiyi.just2@gmail.com>
2016-02-07 15:04:07 +0800

    [AnimatedVector] 学习动态矢量图动画，初始化工程。

---
+++ AnimatedVector/app/src/main/java/me/li2/android/animatedvector/AnimatedVector.java
@@ -0,0 +1,13 @@
+package me.li2.android.animatedvector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnimatedVector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animated_vector);
+    }
+}
