ce35d3d604869f28f0f3479a046b6ba1627cab09
weiyi.li <weiyi.just2@gmail.com>
2016-02-07 17:57:44 +0800

    [AnimatedVector] 增加矢量动画：点击ImageView，在 - 和 + 两个矢量图之间旋转切换。

--- AnimatedVector/app/src/main/java/me/li2/android/animatedvector/AnimatedVector.java
+++ AnimatedVector/app/src/main/java/me/li2/android/animatedvector/AnimatedVector.java
@@ -1,13 +1,38 @@
 package me.li2.android.animatedvector;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AnimatedVector extends AppCompatActivity {
+
+    private ImageView mAddRemoveImage;
+
+    private AnimatedVectorDrawable mAddToRemoveDrawable;
+    private AnimatedVectorDrawable mRemoveToAddDrawable;
+
+    private boolean mIsAddState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animated_vector);
+
+        mAddRemoveImage = (ImageView) findViewById(R.id.image_add_remove);
+        mAddRemoveImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AnimatedVectorDrawable drawable =
+                        mIsAddState ? mRemoveToAddDrawable : mAddToRemoveDrawable;
+                mAddRemoveImage.setImageDrawable(drawable);
+                drawable.start();
+                mIsAddState = !mIsAddState;
+            }
+        });
+
+        mAddToRemoveDrawable = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_add_to_remove);
+        mRemoveToAddDrawable = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_remove_to_add);
     }
 }
