2af9596346d1ecf66e36ae11316d4b79ed010fb2
weiyi.li <weiyi.just2@gmail.com>
2016-02-10 23:50:51 +0800

    [AnimatedVector] 第2个矢量动画，增加细节处理：点击FAB提交答案后，根据答案正确与否，决定切换到勾还是叉，然后隐藏FAB。

--- AnimatedVector/app/src/main/java/me/li2/android/animatedvector/AnimatedVector.java
+++ AnimatedVector/app/src/main/java/me/li2/android/animatedvector/AnimatedVector.java
@@ -1,20 +1,31 @@
 package me.li2.android.animatedvector;
 
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
 import android.graphics.drawable.AnimatedVectorDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class AnimatedVector extends AppCompatActivity {
 
     private ImageView mAddRemoveImage;
-    private CheckableFab mCheckableFab;
-
     private AnimatedVectorDrawable mAddToRemoveDrawable;
     private AnimatedVectorDrawable mRemoveToAddDrawable;
-
     private boolean mIsAddState;
+
+
+    private static final int ANSWER_HIDE_DELAY = 500;
+    private CheckableFab mCheckableFab;
+    private Button mResetFab;
+    private Runnable mHideFabRunnable;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +47,48 @@ public class AnimatedVector extends AppCompatActivity {
         mAddToRemoveDrawable = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_add_to_remove);
         mRemoveToAddDrawable = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_remove_to_add);
 
+
         mCheckableFab = (CheckableFab) findViewById(R.id.checkable_fab);
         mCheckableFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCheckableFab.setChecked(!mCheckableFab.isChecked());
+                Random random = new Random();
+                boolean answerCorrect = (random.nextInt() % 2 == 0);
+                adjustFab(answerCorrect);
             }
         });
+        resetFab();
+
+        mResetFab = (Button) findViewById(R.id.reset_fab);
+        mResetFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetFab();
+            }
+        });
+    }
+
+    private void resetFab() {
+        mCheckableFab.show();
+        final int backgroundColor = ContextCompat.getColor(AnimatedVector.this,
+                android.R.color.holo_blue_dark);
+        mCheckableFab.setChecked(true);
+        mCheckableFab.setBackgroundTintList(ColorStateList.valueOf(backgroundColor));
+    }
+
+    @SuppressLint("NewApi")
+    private void adjustFab(boolean answerCorrect) {
+        final int backgroundColor = ContextCompat.getColor(AnimatedVector.this,
+                answerCorrect ? android.R.color.holo_green_dark : android.R.color.holo_red_light);
+
+        mCheckableFab.setChecked(answerCorrect);
+        mCheckableFab.setBackgroundTintList(ColorStateList.valueOf(backgroundColor));
+        mHideFabRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mCheckableFab.hide();
+            }
+        };
+        mHandler.postDelayed(mHideFabRunnable, ANSWER_HIDE_DELAY);
     }
 }
