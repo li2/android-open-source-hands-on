58685ab18463681b98ae23cf28659249835ba300
weiyi.li <weiyi.just2@gmail.com>
2016-02-14 16:22:05 +0800

    [FloatingActionMenu] 实现 floating action button menu. 开源项目futuresimple/android-floating-action-button实现了浮动按钮菜单，即：点击FAB就会弹出菜单。 这个例子模仿了知乎android客户端的UI。 项目主页：https://github.com/futuresimple/android-floating-action-button

--- FloatingActionMenu/app/src/main/java/me/li2/android/floatingactionmenu/MainActivity.java
+++ FloatingActionMenu/app/src/main/java/me/li2/android/floatingactionmenu/MainActivity.java
@@ -1,7 +1,9 @@
 package me.li2.android.floatingactionmenu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.fab_action_draft).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "clicked Draft floating action button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.fab_action_ask).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "clicked Ask floating action button", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
