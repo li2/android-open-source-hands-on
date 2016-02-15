1fe0bf4545d2c793d46ad48dce20c8a8fa4e8296
weiyi.li <weiyi.just2@gmail.com>
2016-02-15 13:52:27 +0800

    [FloatingActionMenu] 增加一个 mask view 层，在 floating menu 展开时覆盖在当前 activity 之上。

--- FloatingActionMenu/app/src/main/java/me/li2/android/floatingactionmenu/MainActivity.java
+++ FloatingActionMenu/app/src/main/java/me/li2/android/floatingactionmenu/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,6 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final View floatingMaskView = findViewById(R.id.floating_mask);
+        final FloatingActionsMenu floatingActionsMenu = (FloatingActionsMenu) findViewById(R.id.fab_menu);
+        floatingActionsMenu.setOnFloatingActionsMenuUpdateListener(new FloatingActionsMenu.OnFloatingActionsMenuUpdateListener() {
+            @Override
+            public void onMenuExpanded() {
+                floatingMaskView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onMenuCollapsed() {
+                floatingMaskView.setVisibility(View.INVISIBLE);
+            }
+        });
+        
         findViewById(R.id.fab_action_draft).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
