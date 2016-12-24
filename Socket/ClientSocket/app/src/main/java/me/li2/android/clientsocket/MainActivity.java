2dbe2aa7ee1aa8b1e7b75421f55df247ed5f71d3
weiyi.li <weiyi.just2@gmail.com>
2016-12-25 00:21:45 +0800

    [Socket] Add two EditText to allow user to input IP and Port.
    (1) use ButterKnife to find view;
    (2) it would be safer to use HandlerThread instead of Thread in
    ButterKnife onClick listener;

--- Socket/ClientSocket/app/src/main/java/me/li2/android/clientsocket/MainActivity.java
+++ Socket/ClientSocket/app/src/main/java/me/li2/android/clientsocket/MainActivity.java
@@ -1,21 +1,30 @@
 package me.li2.android.clientsocket;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "ClientSocket";
     private static final String HELLO_WORLD = "Hello World, I'm an Android Client-Side Socket";
-    private TextView mMessageView;
+
+    @BindView(R.id.messageText) TextView mMessageView;
+    @BindView(R.id.serverIpEditText) EditText mIpEditText;
+    @BindView(R.id.serverPortEditText) EditText mPortEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,31 +32,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        mMessageView = (TextView) findViewById(R.id.messageText);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        send(HELLO_WORLD);
-                    }
-                }, "SocketThread").start();
-            }
-        });
+        ButterKnife.bind(this);
     }
 
-    private static final String NETWORK_IP = "192.168.0.100";
-    private static int NETWORK_PORT = 3000;
+    @OnClick(R.id.connectBtn)
+    void connectToServer() {
 
-    private void send(String packet)
-    {
+    }
+
+    @OnClick(R.id.sendBtn)
+    void sendToServer() {
+        HandlerThread thread = new HandlerThread("SocketThread");
+        thread.start();
+        Handler handler = new Handler(thread.getLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "send to server", Toast.LENGTH_LONG).show();
+                send(HELLO_WORLD);
+            }
+        });
+        /*
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Toast.makeText(MainActivity.this, "sendToServer", Toast.LENGTH_LONG).show();
+                // RuntimeException: Can't create handler inside thread that has not called Looper.prepare()
+                // NOTE21: Call toast in this thread will cause exception!
+                send(HELLO_WORLD);
+            }
+        }, "SocketThread").start();
+        */
+    }
+
+    private static final String SERVER_IP = "192.168.0.102";
+    private static int SERVER_PORT = 3000;
+
+    private void send(String packet) {
         try {
             Log.d(TAG, "client socket");
-            Socket socket = new Socket(NETWORK_IP, NETWORK_PORT);
+            String ip = mIpEditText.getText().toString();
+            int port = Integer.valueOf(mPortEditText.getText().toString());
+
+            Socket socket = new Socket(ip, port);
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
             dataOutputStream.writeUTF(packet);
             dataOutputStream.flush();
